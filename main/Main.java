143f360f9d78c1f71c8549521d30273ffbe90281
Luka <lkurdgelaidze@gmail.com>
2024-12-29 04:58:44 +0400

    Added variable declaration recognition.

--- main/Main.java
+++ main/Main.java
@@ -7,7 +7,14 @@ public class Main {
     public static void main(String[] args) {
         String rubyCode= "main/ruby.rb";
         for (String line: RubyToJavaInterpreter.textFileToStringArray(rubyCode)) {
+            RubyToJavaInterpreter.findVariableDeclaration(line);
             System.out.println(line);
         }
+
+        System.out.println("\n");
+
+        RubyToJavaInterpreter.variableMap.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
     }
 }
--- main/RubyToJavaInterpreter.java
+++ main/RubyToJavaInterpreter.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
+import java.util.HashMap;
 
 public class RubyToJavaInterpreter {
+
+    public static HashMap<String, Object> variableMap = new HashMap<>();
+
     public static String[]textFileToStringArray(String fileName){
         ArrayList<String> rubyLineArrayList = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
@@ -21,5 +26,46 @@ public class RubyToJavaInterpreter {
         rubyLineArrayList.toArray(rubyLinesArray);
 
         return rubyLinesArray;
+    }
+
+    public static void findVariableDeclaration (String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '=' && line.charAt(i + 1) != '=') {
+                String variableName = "";
+                String variableValue = "";
+
+                // Ignoring optional space
+                int j = i - 1;
+                if (line.charAt(i - 1) == ' ') j--;
+
+                // Getting variable name
+                while (j >= 0 && line.charAt(j) != ' ') {
+                    variableName += line.charAt(j);
+                    j --;
+                }
+
+                variableName = new StringBuilder(variableName).reverse().toString();
+
+                int k = i + 1;
+                if (line.charAt(k) == ' ') k++;
+
+                while (k < line.length() && line.charAt(k) != ' ') {
+                    variableValue += line.charAt(k);
+                    k++;
+                }
+
+                if (variableValue.charAt(0) == '"' || variableValue.charAt(0) == '\'') {
+                    variableMap.put(variableName, variableValue.substring(1, variableValue.length() - 1));
+                } else if (variableValue.charAt(0) == 't') {
+                    variableMap.put(variableName, true);
+                } else if (variableValue.charAt(0) == 'f') {
+                    variableMap.put(variableName, false);
+                } else if (variableValue.contains(".")) {
+                    variableMap.put(variableName, Double.parseDouble(variableValue));
+                } else {
+                    variableMap.put(variableName, Integer.parseInt(variableValue));
+                }
+            }
+        }
     }
 }
